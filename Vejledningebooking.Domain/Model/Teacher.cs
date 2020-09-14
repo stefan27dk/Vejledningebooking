31c4c73a6e6c54f66fccac5d1267e5b108123019
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-09-14 19:36:36 +0200

    Add project files.

---
+++ Vejledningebooking.Domain/Model/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vejledningebooking.Domain.Model
+{
+    class Teacher
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        
+
+
+
+    }
+}
---
+++ Vejledningebooking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vejledningebooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
